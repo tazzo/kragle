40471eb08c68540aab2a7f2f02b37abbfd3b4b6c
tazzo <roberto.tazzoli@gmail.com>
2021-01-25 23:31:28 +0100

    trading starting layout

--- app_trading.py
+++ app_trading.py
@@ -2,5 +2,23 @@ import dash_html_components as html
 
 
 def render_trading_page():
-    return html.Div(children=[html.P('Trading page'),
-                              html.Div(id='trading-content')])
+    return html.Div(
+        className="bg-white font-sans leading-normal tracking-normal mt-12 w-full",
+        children=[
+            html.Div(
+                className="box-container",
+                children=[
+                    html.Div(
+                        className='w-full p-2',
+                        children=html.Div(
+                            className='box',
+                            children=[
+                                "Trading dashboard"
+                            ],
+                        ),
+                    ),
+
+                ],
+            ),
+        ]
+    )
